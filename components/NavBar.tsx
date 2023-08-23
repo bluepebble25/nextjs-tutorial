6734bab16ac02d588e96b4bf7dc9f9add6271934
bluepebble25 <bluepebble25@gmail.com>
2023-08-24 00:21:32 +0900

    design: Navbar 스타일링

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,15 +5,38 @@ function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/" legacyBehavior>
-        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
-      </Link>
-      <Link href="/about" legacyBehavior>
-        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/" legacyBehavior>
+          <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
+        </Link>
+      </div>
+
       <style jsx>{`
-        a {
-          text-decoration: none;
+        nav {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 10px;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 10px;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
           color: tomato;
