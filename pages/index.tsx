5f0db9d652ec7042717d73b10b6febfe3fa3cbf4
bluepebble25 <bluepebble25@gmail.com>
2023-08-25 20:50:16 +0900

    design: 영화 리스트 스타일링

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,42 @@ function Home() {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       {!movies.length && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div key={movie.id}>
+      {movies?.map((movie: any) => (
+        <div className="movie" key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={`${movie.original_title}`}
+          />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          gap: 20px;
+          padding: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.3) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          margin-top: 10px;
+          text-align: center;
+          font-size: 18px;
+        }
+      `}</style>
     </div>
   );
 }
